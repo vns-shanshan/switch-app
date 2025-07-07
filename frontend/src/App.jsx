bd03d8fc789f8b4408d475e8082fb587b385b7ed
huijingj <vanessajin0426@gmail.com>
2025-07-07 10:52:47 -0700

    2 - Frontend Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+
+import HomePage from "./pages/HomePage.jsx";
+import AuthPage from "./pages/AuthPage.jsx";
+import ProfilePage from "./pages/ProfilePage.jsx";
+import ChatPage from "./pages/ChatPage.jsx";
+
+function App() {
+  return (
+    <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/chat/:id" element={<ChatPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
